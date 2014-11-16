d08e5fdd1df66e39fdd7b39e7d5b78b171649ca5
Diego Ramallo <ramalldf@stanford.edu>
2014-11-16 01:44:05 -0800

    Update 11/16

---
+++ medianCdf.m
@@ -0,0 +1,27 @@
+%Finds the nanmedian of your population. Adds a cdf plot as an optional arg
+%argument 1: matrix or vector
+%argument 2: 'Color' of your cdf plot that will be generated
+%ie. [medLifetime]= medianCdf(matrix, 'green');
+
+function [med]= medianCdf(varargin)% Varargin allows for optional arguments
+if length(varargin)==1 %Case with one argument
+    data= varargin{1};%Excludes NaNs
+    med= nanmedian(data(:));
+elseif length(varargin)==2 %Case with two arguments
+    data= varargin{1};
+    med= nanmedian(data(:));
+    
+    %%=cdf plot=
+matrixList= data(:);
+matrixList= matrixList(find(matrixList>0));
+sortedList= sort((matrixList(:)));
+intervals= [1./length(sortedList):1./length(sortedList):1];
+plot(sortedList, intervals,'color',varargin{2},'LineWidth',2)
+xlabel('Lifetime (ps)')
+ylabel('Probability')
+xlim([0 med+med*.5])
+ylim([0 1])
+    
+end
+
+    
